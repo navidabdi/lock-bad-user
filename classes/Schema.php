40a82e0d60527774240ffe25cecbcb1270664eda
Nabi <32398454+navidabdi@users.noreply.github.com>
2023-08-04 14:27:28 +0330

    Added Inpsyde coding standards to the plugin. (#1)

--- classes/Schema.php
+++ classes/Schema.php
@@ -9,9 +9,9 @@ class Schema
   /**
    * Activate the plugin schema.
    */
-  public static function activate(): void
-  {
-  }
+    public static function activate(): void
+    {
+    }
 
   /**
    * Deactivate the plugin schema.
@@ -19,9 +19,9 @@ class Schema
    * This method is called when the plugin is deactivated.
    * It flushes the rewrite rules.
    */
-  public static function deactivate(): void
-  {
-  }
+    public static function deactivate(): void
+    {
+    }
 
   /**
    * Uninstall the plugin schema.
@@ -29,8 +29,8 @@ class Schema
    * This method is called when the plugin is uninstalled.
    * It performs any necessary cleanup or data removal operations.
    */
-  public static function uninstall(): void
-  {
-    // Perform uninstallation tasks, if any.
-  }
+    public static function uninstall(): void
+    {
+      // Perform uninstallation tasks, if any.
+    }
 }
