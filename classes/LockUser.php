3872b4fa428413e1c7c5293b75fd5c3d0d2eea32
Nabi <32398454+navidabdi@users.noreply.github.com>
2023-08-04 18:18:11 +0330

    Fixed code standards (version one). (#4)

--- classes/LockUser.php
+++ classes/LockUser.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Webkima\LockBadUser;
 
+use WP_Error;
+use WP_User;
+
 class LockUser
 {
     public function register(): void
@@ -19,50 +22,59 @@ class LockUser
         add_filter('allow_password_reset', [$this, 'allowPasswordReset'], 9999, 2);
     }
 
-    private function isUserLocked($user_id): bool
+    private function isUserLocked(string $userId): bool
     {
-        return (bool) get_user_meta($user_id, 'account_locked', true);
+        return (bool) get_user_meta($userId, 'account_locked', true);
     }
 
-    public function outputLockStatusOptions($user): void
+    public function outputLockStatusOptions(WP_User $user): void
     {
-        $locking_data = $this->isUserLocked($user->data->ID);
+        $isUserLocked = $this->isUserLocked($user->data->ID);
         require_once LOCK_BAD_USER_PATH . 'templates/output_lock_status_options.php';
     }
 
-    public function saveLockStatusOption($user_id): void
+    public function saveLockStatusOption(int $userId): void
     {
+        // phpcs:disable WordPress.Security.NonceVerification
         if (isset($_POST['account_status'])) {
-            if ($_POST['account_status'] == 'locked') {
-                $this->lockAccount($user_id);
-            } else {
-                $this->unlockAccount($user_id);
+            if ($_POST['account_status'] === 'locked') {
+                $this->lockAccount($userId);
+            }
+            if ($_POST['account_status'] === 'unlocked') {
+                $this->unlockAccount($userId);
             }
         }
     }
 
-    public function lockAccount($user_id): void
+    public function lockAccount(int $userId): void
     {
-        update_user_meta($user_id, 'account_locked', true);
+        update_user_meta($userId, 'account_locked', true);
 
-        self::kickOutBadUser($user_id);
+        self::kickOutBadUser($userId);
 
-        do_action('lock_bad_user_lock_account', $user_id);
+        do_action('lock_bad_user_lock_account', $userId);
     }
 
-    public function unlockAccount($user_id): void
+    public function unlockAccount(int $userId): void
     {
-        delete_user_meta($user_id, 'account_locked');
-        do_action('lock_bad_user_unlock_account', $user_id);
+        delete_user_meta($userId, 'account_locked');
+        do_action('lock_bad_user_unlock_account', $userId);
     }
 
+    /**
+     * Check if user is locked and kicked out.
+     *
+     * @param $user
+     *
+     * @return WP_Error|WP_User
+     * phpcs:disable
+     */
     public function userAuthentication($user)
     {
         $status = get_class($user);
-        if ($status == 'WP_User') {
-            $locking_data = $this->isUserLocked($user->data->ID);
-
-            if ($locking_data) {
+        if ($status === 'WP_User') {
+            $isUserLocked = $this->isUserLocked($user->data->ID);
+            if ($isUserLocked) {
                 $message = apply_filters(
                     'account_lock_message',
                     sprintf(
@@ -73,27 +85,24 @@ class LockUser
                     $user
                 );
                 return new \WP_Error('authentication_failed', $message);
-            } else {
-                return $user;
             }
+          return $user;
         }
         return $user;
     }
 
-    public function allowPasswordReset($status, $user_id)
+    public function allowPasswordReset(bool $status, int $userId): bool
     {
-        $locking_data = $this->isUserLocked($user_id);
-        if ($locking_data) {
+        $isUserLocked = $this->isUserLocked((string) $userId);
+        if ($isUserLocked) {
             return false;
-        } else {
-            return $status;
         }
+        return $status;
     }
 
-    public static function kickOutBadUser($user_id): void
+    public static function kickOutBadUser(int $userId): void
     {
-        $sessions = \WP_Session_Tokens::get_instance($user_id);
-
+        $sessions = \WP_Session_Tokens::get_instance($userId);
         $sessions->destroy_all();
     }
 
