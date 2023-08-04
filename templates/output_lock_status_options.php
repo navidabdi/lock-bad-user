3e7b6879d1ce7653424c41f4ba4d609834ce1740
Nabi <artsanaat@gmail.com>
2023-08-04 17:42:10 +0330

    Fixed code standards (version one).

--- templates/output_lock_status_options.php
+++ templates/output_lock_status_options.php
@@ -1,11 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+?>
+
 <h3>Lock User Account</h3>
 <table class="form-table">
     <tr>
         <th><label for="account_status">Account status</label></th>
         <td>
             <select name="account_status" id="account_status">
-                <option value="unlocked" <?php selected(!$locking_data) ?> >Unlocked</option>
-                <option value="locked" <?php selected($locking_data) ?> >Locked</option>
+                <?php // phpcs:disable ?>
+                <option value="unlocked" <?php selected(!$isUserLocked) ?> >Unlocked</option>
+                <option value="locked" <?php selected($isUserLocked) ?> >Locked</option>
             </select>
         </td>
     </tr>
